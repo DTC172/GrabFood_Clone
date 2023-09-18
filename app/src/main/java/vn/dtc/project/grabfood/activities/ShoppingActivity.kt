0e7d4784f1d2c7057b08925e1ca0419d492b382a
ThanhCong <doanthanhcong17@gmail.com>
2023-09-18 17:25:57 +0700

    + bottom nav

--- app/src/main/java/vn/dtc/project/grabfood/activities/ShoppingActivity.kt
+++ app/src/main/java/vn/dtc/project/grabfood/activities/ShoppingActivity.kt
@@ -2,11 +2,23 @@ package vn.dtc.project.grabfood.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import vn.dtc.project.grabfood.R
 import vn.dtc.project.grabfood.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
 
 }
